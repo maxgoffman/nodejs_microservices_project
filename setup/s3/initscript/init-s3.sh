b51f2c263401e687c2ffefcc596b9f679af7fee4
Maximiliano Goffman <maxgoffman@gmail.com>
2025-02-21 09:00:55 -0300

    Removed quotes.

--- setup/s3/initscript/init-s3.sh
+++ setup/s3/initscript/init-s3.sh
@@ -2,7 +2,7 @@
 
 # Create the S3 bucket
 echo "creating documents-bucket"
-awslocal s3api create-bucket --bucket "documents-bucket"
+awslocal s3api create-bucket --bucket documents-bucket
 
 # Upload the initial document
 echo "uploading file to documents-bucket"
